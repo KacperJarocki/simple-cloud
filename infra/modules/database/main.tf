67eea6e7a5491dc972ee0cce96b15e68712ea2b5
Kacper Jarocki <dev.kacperjarocki@proton.me>
2025-05-17 15:44:37 +0200

    added private dns zone id

--- infra/modules/database/main.tf
+++ infra/modules/database/main.tf
@@ -11,27 +11,23 @@ resource "azurerm_private_dns_zone_virtual_network_link" "network_link" {
 }
 
 resource "azurerm_postgresql_flexible_server" "db_server" {
-  name                = "${var.env}-${var.project}-database-server"
-  location            = var.location
-  resource_group_name = var.rg_name
-
-  sku_name                     = "B_Standard_B1ms"
-  version                      = "11"
-  storage_mb                   = 32768
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-
-  administrator_login    = "psqladmin"
-  administrator_password = var.postgres_password
-
-  delegated_subnet_id = var.subnet_id
+  name                          = "${var.env}-${var.project}-database-server"
+  location                      = var.location
+  resource_group_name           = var.rg_name
+  sku_name                      = "B_Standard_B1ms"
+  version                       = "11"
+  storage_mb                    = 32768
+  backup_retention_days         = 7
+  geo_redundant_backup_enabled  = false
+  private_dns_zone_id           = azurerm_private_dns_zone.database_zone.id
+  administrator_login           = "psqladmin"
+  administrator_password        = var.postgres_password
+  delegated_subnet_id           = var.subnet_id
+  public_network_access_enabled = false
 
   high_availability {
     mode = "SameZone"
   }
-
-  public_network_access_enabled = false
-
   tags = {
     environment = var.env
   }
