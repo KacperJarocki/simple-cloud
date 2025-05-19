de13a2d1574dad6e9baf76f8dca1a2f8d12c3500
Kacper Jarocki <dev.kacperjarocki@proton.me>
2025-05-19 13:02:31 +0200

    fix: azurerm_user_assingned_identity

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -12,14 +12,41 @@ resource "azurerm_service_plan" "plan" {
 }
 
 
+resource "azurerm_user_assigned_identity" "uai" {
+  name                = "${var.env}-${var.project}-uai"
+  resource_group_name = var.rg_name
+  location            = var.location
+  tags = {
+    environment = var.env
+  }
+}
+
+resource "azurerm_key_vault_access_policy" "uai_policy" {
+  key_vault_id = var.keyvault_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_user_assigned_identity.uai.principal_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+  ]
+}
+
 resource "azurerm_linux_web_app" "web_app" {
   name                = "${var.env}-${var.project}-web-app"
   resource_group_name = var.rg_name
   location            = var.location
   service_plan_id     = azurerm_service_plan.plan.id
+
   identity {
-    type = "SystemAssigned"
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.uai.id,
+    ]
   }
+
+  key_vault_reference_identity_id = azurerm_user_assigned_identity.uai.id
+
   app_settings = {
     "DB_USER" = "@Microsoft.KeyVault(SecretUri=${var.db_user_secret_id})"
     "DB_PASS" = "@Microsoft.KeyVault(SecretUri=${var.db_pass_secret_id})"
@@ -27,35 +54,19 @@ resource "azurerm_linux_web_app" "web_app" {
     "DB_PORT" = "5432"
     "DB_NAME" = "${var.env}-${var.project}-database"
   }
-  depends_on = [
-    azurerm_key_vault_access_policy.appservice_policy
-  ]
-  site_config {
 
+  site_config {
     application_stack {
       docker_image_name   = "${var.docker_image_name}:${var.docker_image_tag}"
       docker_registry_url = "https://${var.docker_registry_url}"
     }
   }
+
   tags = {
     environment = var.env
   }
-}
-resource "azurerm_app_service_virtual_network_swift_connection" "connection" {
-  app_service_id = azurerm_linux_web_app.web_app.id
-  subnet_id      = var.subnet_id
-}
-
-resource "azurerm_key_vault_access_policy" "appservice_policy" {
-  key_vault_id = var.keyvault_id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_linux_web_app.web_app.identity[0].principal_id
 
   depends_on = [
-    azurerm_linux_web_app.web_app
-  ]
-  secret_permissions = [
-    "Get",
-    "List"
+    azurerm_key_vault_access_policy.uai_policy
   ]
 }
